0c8dd86c169514a6d6af352aa21295ba9b71f287
NuffSaid-Bore <borekamohelo@gmail.com>
2025-02-14 09:12:06 +0200

    changed the structure of the test code

--- src/test/java/com/java/junit/CalculatorTest.java
+++ src/test/java/com/java/junit/CalculatorTest.java
@@ -8,47 +8,95 @@ import org.junit.Test;
  */
 public class CalculatorTest 
 {
-   // Arrange
-
-   int f = -1;
-   int a = 1;
-   int b = 2;
-   int c = 3;
-   int d = 4;
-   int e = 5;
-
-   // Act
-   int one_plus_two = Calculator.add(a , b);
-   int negative_one_plus_negative_one = Calculator.add(f , f);
-   int one_to_five_add = Calculator.add(a , b , c , d , e);
-   int multiply_one_by_three = Calculator.multiply(a , c);
-   int multiply_neg_one_three = Calculator.multiply(f , c);
-   int multiply_one_to_five = Calculator.multiply(a, b, c, d, e);
-
-
-   // Assert
 
     @Test
-    public void addTest(){
+    public void addTwoPositiveNumbersTest(){
+
+        // Arrange
+        int a = 1;
+        int b = 2;
+
+        // Act
+        int one_plus_two = Calculator.add(a , b);
+
+        // Assert
         assertEquals(3, one_plus_two);
+    }
+
+        @Test
+    public void addTwoNegativeNumbersTest(){
+
+         // Arrange
+        int f = -1;
+
+        // Act
+        int negative_one_plus_negative_one = Calculator.add(f , f);
+
+        // Assert
         assertEquals(-2, negative_one_plus_negative_one);
     }
 
     @Test 
     public void addMultipleNumbersTest(){
 
+        // Arrange
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+
+        // Act
+        int one_to_five_add = Calculator.add(a , b , c , d , e);
+
+        // Assert
         // using the modified method
         assertEquals(15, one_to_five_add);
     }
 
     @Test
-    public void multiplyTest(){
+    public void multiplyTwoPositiveNumbersTest(){
+
+        // Arrange
+        int a = 1;
+        int c = 3;
+        
+
+        // Act
+        int multiply_one_by_three = Calculator.multiply(a , c);
+
+        // Assert
         assertEquals(3, multiply_one_by_three);
-        assertEquals(-3, multiply_neg_one_three);
     }
 
     @Test
+    public void multiplyNegativeByThreeTest(){
+
+        // Arrange
+        int f = -1;
+        int c = 3;
+
+        // Act
+        int multiply_neg_one_three = Calculator.multiply(f , c);
+        
+        // Assert
+        assertEquals(-3, multiply_neg_one_three);
+    }
+
+    
+    @Test
     public void multiplyMultipleNumbersTest(){
+        // Arrange
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+
+        // Act
+        int multiply_one_to_five = Calculator.multiply(a, b, c, d, e);
+
+        // Assert
         // using the modified method
         assertEquals(120, multiply_one_to_five);
     }
